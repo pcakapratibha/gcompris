669f0f319d3d3c00e0e4e217ccd3e0e46f083eb7
Bruno Coudoin <bcoudoin@src.gnome.org>
2006-11-10 23:34:02 +0000

    - Backport from HEAD: 	  Quiting the gnumch or searace activity crash GCompris 	  1 times on 10.
    	- Minor:
    	- new interrupt audio mode to have a more reactive experience
    	in audio activity. Audio memory and then melody activities
    	are now much more pleasant to play (Yves fix on a Markus request).

--- src/boards/python/melody.py
+++ src/boards/python/melody.py
@@ -48,6 +48,9 @@ class Gcompris_melody:
 
 
   def start(self):
+    self.saved_policy = gcompris.sound.policy_get()
+    gcompris.sound.policy_set(gcompris.sound.PLAY_AND_INTERRUPT)
+
     self.gcomprisBoard.level=1
     self.gcomprisBoard.sublevel=1
     self.gcomprisBoard.number_of_sublevel=1
@@ -117,6 +120,7 @@ class Gcompris_melody:
   def end(self):
     self.cleanup()
     print("Gcompris_melody end.")
+    gcompris.sound.policy_set(self.saved_policy)
     gcompris.sound.resume()
 
 
@@ -376,10 +380,10 @@ class Gcompris_melody:
     return False
 
   def sound_played(self, file):
-    print "python sound played :", file
+    #print "python sound played :", file
 
   def intro_cb(self, file):
-    print "intro passed. go play"
+    #print "intro passed. go play"
     self.pause(0)
     self.populate(self.sound_list)
 
