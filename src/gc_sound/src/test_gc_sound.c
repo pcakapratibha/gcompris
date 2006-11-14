55fe11f8ccbaba4e907264bd904469b696b0aeac
Yves Combe <ycombe@src.gnome.org>
2006-11-14 21:59:18 +0000

    gc_sound: play groups of item

--- src/gc_sound/src/test_gc_sound.c
+++ src/gc_sound/src/test_gc_sound.c
@@ -4,42 +4,70 @@
 
 void finish(GCSoundItem *item, gpointer data)
 {
-  g_warning("Return of finish callback %s", gc_sound_item_get_filename(item));
+  g_warning("Return of finish callback %s", item->nick);
+}
+
+void start(GCSoundItem *item, gpointer data)
+{
+  g_warning("Return of start callback %s", item->nick);
 }
 
 main (int argc, char *argv)
 {
   GCSoundMixer *gcmix;
   GCSoundChannel* gcchan1, *gcchan2;
-  GCSoundItem *item1, *item2, *root1, *root2;
+  GCSoundItem *item1, *item2, *root1, *root2, *item1_1, *item1_2;
   g_type_init ();
 
-  gcmix = GC_SOUND_MIXER(gc_sound_mixer_new());
+  /* Sound mixer opens SDL and audio device */
+  gcmix = gc_sound_mixer_new();
+  gcmix->nick = "gcmix";
 
+  /* We open two chans to play simultaneously */
   gcchan1 = gc_sound_mixer_new_channel(gcmix);
   gcchan2 = gc_sound_mixer_new_channel(gcmix);
+  gcchan1->nick = "gcchan1";
+  gcchan2->nick = "gcchan2";
 
-  printf ("gcchan1 %d gcchan2 %d\n", gcchan1, gcchan2);
 
+  /* Each chan has a root item, where we group the sounds to play in that channel*/
   root1 = GC_SOUND_ITEM(gc_sound_channel_get_rootItem(gcchan1));
   root2 = GC_SOUND_ITEM(gc_sound_channel_get_rootItem(gcchan2));
+  root1->nick="root1";
+  root2->nick="root2";
 
-  printf ("root1 %d root2 %d\n", root1, root2);
+  /* create new item for our sounds */
+   item1 = gc_sound_item_new (root1);
+   item2 = gc_sound_item_new (root2);
+   item1_1 = gc_sound_item_new (item1);
 
-   item1 = GC_SOUND_ITEM(gc_sound_item_new (root1));
-   item2 = GC_SOUND_ITEM(gc_sound_item_new (root2));
+   item1_2 = gc_sound_item_new (item1);
+   item1->nick = "item1";
+   item2->nick = "item2";
+   item1_1->nick = "item1_1";
+   item1_2->nick = "item1_2";
 
-  printf ("item1 %d item2 %d\n", item1, item2);
+   /* set file sound name */
+   gc_sound_item_set_filename(item1_1, "../../../boards/sounds/wahoo.ogg");
+   gc_sound_item_set_filename(item1_2, "../../../boards/sounds/wahoo.ogg");
+   gc_sound_item_set_filename(item2, "../../../boards/music/background/LRLaPause.ogg");
 
-   gc_sound_item_set_filename(item1, "wahoo.ogg");
-   gc_sound_item_set_filename(item2, "LRLaPause.ogg");
+   // test
+   gc_sound_item_set_filename(item1_1, NULL);
+   gc_sound_item_set_filename(item1_1, "../../../boards/sounds/wahoo.ogg");
 
-   gc_sound_item_play(item2, -1);
-   gc_sound_item_play(item1, -1);
-
+   // callback connection
    gc_sound_item_connect__play_finished(item1, finish, NULL);
    gc_sound_item_connect__play_finished(item2, finish, NULL);
+   gc_sound_item_connect__play_started(item1_1, start, NULL);
+   gc_sound_item_connect__play_started(item1_2, start, NULL);
 
+   //play
+   gc_sound_item_play(item2);
+   g_usleep(1000000);
+   gc_sound_item_play(item1);
+
+   /* infinite loop */
    g_main_loop_run(g_main_loop_new  (NULL, FALSE));
 
 }
