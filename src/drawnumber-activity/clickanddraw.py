99ac513f95147fbc23e2cd77a49115d7442b60c1
Bruno Coudoin <bcoudoin@src.gnome.org>
2007-12-26 21:13:11 +0000

    Fixed clickanddraw not to fail at level change Fixed drawnumber to run properly now. the init_path was wrong
    svn path=/branches/gcomprixo/; revision=3178

--- src/drawnumber-activity/clickanddraw.py
+++ src/drawnumber-activity/clickanddraw.py
@@ -45,7 +45,8 @@ class Gcompris_clickanddraw(Gcompris_drawnumber) :
       )
 
     #Setting of the first background image of the level
-    gcompris.set_background(self.ROOT,self.data[sublevel][0][1])
+    gcompris.set_background(self.gcomprisBoard.canvas.root(),
+                            self.data[sublevel][0][1])
 
     #Initialisation of sub-elements in list
     self.POINT=[0]
