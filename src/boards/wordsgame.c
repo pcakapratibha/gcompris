77740705113746ec97b3dccc5f5bd18563b0a80e
Bruno Coudoin <bcoudoin@src.gnome.org>
2008-02-17 23:47:05 +0000

    now put libxf86vm in a separate variable and no more use it to compile
    	* configure.in: now put libxf86vm in a separate variable and no more use
    	  it to compile plugins (only core need it).
    	* src/gcompris/Makefile.am: -
    	* boards/voices/nn: created a link to nb voices until we get a voice set.
    
    
    svn path=/trunk/; revision=3262

--- src/boards/wordsgame.c
+++ src/boards/wordsgame.c
@@ -1,7 +1,5 @@
 /* gcompris - wordsgame.c
  *
- * Time-stamp: <2007-08-22 01:27:15 bruno>
- *
  * Copyright (C) 2000 Bruno Coudoin
  *
  *   This program is free software; you can redistribute it and/or modify
