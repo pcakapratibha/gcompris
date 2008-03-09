83c2a44656ed2559b4cb302b87fcbadd2c0b0b61
Bruno Coudoin <bcoudoin@src.gnome.org>
2008-03-09 23:31:21 +0000

    Made the chess activity to search for gnuchess in usual 	directory if it is not found in the one detected at compilation 	time. It is done to accomodate cases were the run time and build 	time have a different gnuchess package.
    svn path=/branches/gcomprixo/; revision=3284

--- src/gcompris/gcompris.c
+++ src/gcompris/gcompris.c
@@ -1066,6 +1066,14 @@ static void load_properties ()
   else
     g_warning("Binary relocation disabled");
 
+  /* usefull for OSX bundle app */
+#ifdef NSBUNDLE
+  exec_prefix = gcompris_nsbundle_resource ();
+#else
+  exec_prefix = gbr_find_exe_dir("");
+#endif
+  g_warning("exec_prefix %s\n", exec_prefix);
+
   prefix_dir = gbr_find_prefix(NULL);
   g_warning("prefix_dir=%s\n", prefix_dir);
 
@@ -1286,15 +1294,6 @@ main (int argc, char *argv[])
 {
   GError *error = NULL;
   GOptionContext *context;
-
-  /* usefull for OSX bundle app */
-#ifdef NSBUNDLE
-  exec_prefix = gcompris_nsbundle_resource ();
-  printf("exec_prefix %s\n", exec_prefix);
-#else
-  exec_prefix = NULL;
-  printf("exec_prefix NULL\n");
-#endif
 
   /* First, Remove the gnome crash dialog because it locks the user when in full screen */
   signal(SIGSEGV, gc_terminate);
