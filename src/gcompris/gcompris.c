3c5e645e63e6dac2e7695eaed54d6ca13bf731ea
Bruno Coudoin <bcoudoin@src.gnome.org>
2008-05-02 20:56:09 +0000

    Touched texte in source file to match trunk so that 	we can take po files from trunk in this branch. 	Updated po files from trunk.
    svn path=/branches/GCOMPRIS_8_3/; revision=3395

--- src/gcompris/gcompris.c
+++ src/gcompris/gcompris.c
@@ -134,19 +134,19 @@ static gchar *popt_drag_mode       = NULL;
 
 static GOptionEntry options[] = {
   {"fullscreen", 'f', 0, G_OPTION_ARG_NONE, &popt_fullscreen,
-   N_("run gcompris in fullscreen mode."), NULL},
+   N_("run GCompris in fullscreen mode."), NULL},
 
   {"window", 'w', 0, G_OPTION_ARG_NONE, &popt_window,
-   N_("run gcompris in window mode."), NULL},
+   N_("run GCompris in window mode."), NULL},
 
   {"sound", 's', 0, G_OPTION_ARG_NONE, &popt_sound,
-   N_("run gcompris with sound enabled."), NULL},
+   N_("run GCompris with sound enabled."), NULL},
 
   {"mute", 'm', 0, G_OPTION_ARG_NONE, &popt_mute,
-   N_("run gcompris without sound."), NULL},
+   N_("run GCompris without sound."), NULL},
 
   {"cursor", 'c', 0, G_OPTION_ARG_NONE, &popt_cursor,
-   N_("run gcompris with the default gnome cursor."), NULL},
+   N_("run GCompris with the default gnome cursor."), NULL},
 
   {"difficulty", 'd', 0, G_OPTION_ARG_INT, &popt_difficulty_filter,
    N_("display only activities with this difficulty level."), NULL},
@@ -164,7 +164,7 @@ static GOptionEntry options[] = {
    N_("Disable XF86VidMode (No screen resolution change)."), NULL},
 
   {"root-menu", 'l', 0, G_OPTION_ARG_STRING, &popt_root_menu,
-   N_("Run gcompris with local menu (e.g -l /reading will let you play only activities in the reading directory, -l /strategy/connect4 only the connect4 activity)"), NULL},
+   N_("Run GCompris with local menu (e.g -l /reading will let you play only activities in the reading directory, -l /strategy/connect4 only the connect4 activity)"), NULL},
 
   {"local-activity", 'L', 0, G_OPTION_ARG_STRING, &popt_local_activity,
    N_("Run GCompris with local activity directory added to menu"), NULL},
